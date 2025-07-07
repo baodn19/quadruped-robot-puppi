c6abd3ab93d25512705d4f9f8edf46b26465d8a3
baodn19 <baodn19@gmail.com>
2025-07-07 14:40:05 +0700

    Control motor with steps

--- Servo Control/PCA9685_Servo3/PCA9685_Servo3.ino
+++ Servo Control/PCA9685_Servo3/PCA9685_Servo3.ino
@@ -2,10 +2,11 @@
 
 TwoWire I2C = TwoWire(0); // Create a TwoWire object for I2C communication
 Servo1 pwm = Servo1(PCA9685_I2C_ADDRESS, I2C); // Create a Servo1 object with the default I2C address
-#define SCL_PIN 32
-#define SDA_PIN 33
+#define SCL_PIN 33
+#define SDA_PIN 32
 #define SERVO_REFRESH_RATE 50 // 50Hz for servos
 int initial_position = 86; // 45 degrees
+int position = 86;
 
 void InitiateServo() {
   I2C.begin(SDA_PIN, SCL_PIN, 26000000); // Initialize I2C with SDA and SCL pins
@@ -19,6 +20,7 @@ void InitiateServo() {
 void ServoStep(byte servo_ID, int step) {
   for(int i = initial_position; i <= 484; i += step) {
     pwm.setPWM(servo_ID, 0, i); // Set the servo position
+    delay(50);
   }
 }
 
@@ -31,5 +33,4 @@ void setup() {
 void loop() {
   // Example usage of ServoStep
   ServoStep(0, 1);
-  delay(50);
 }
