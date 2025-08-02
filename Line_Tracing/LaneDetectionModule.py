d52a70ac235e6d210dd3debe7ac633aa7d7ec0bd
baodn19 <baodn19@gmail.com>
2025-08-02 11:22:13 +0700

    find initial value for warping

--- Line_Tracing/LaneDetectionModule.py
+++ Line_Tracing/LaneDetectionModule.py
@@ -8,7 +8,7 @@ def get_lane_curve(image):
 
     height, width = image.shape[:2]
     points = ut.trackbars_value()
-    image_warped = ut.perspective_warp(image_threshold, points, width, height)
+    image_warped = ut.perspective_warp(image, points, width, height)
     image_points = ut.draw_points(image, points)
 
     cv2.imshow("Thresholded Image", image_threshold)
