5ee30fc941a07dbb9c41fccfb9413e42a61c8280
baodn19 <baodn19@gmail.com>
2025-08-07 16:30:50 +0700

    Move back to original folder

---
+++ Line_Tracing/LaneDetectionModule.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import utilities as ut
+
+curve_list = []
+average_curve_value = 10  # Number of frames to average the curve over
+
+def get_lane_curve(image, display=2):
+    """
+    Description: This function processes an image to detect lane curves by applying a series of transformations and analyses.
+    Parameters:
+    - image (numpy.3darray): The input image from which lane curves are to be detected.
+    - display (int): The display mode for the output. Default is 2, which shows the everything.1 shows the resulting curve, and 0 shows nothing.
+    """
+    image_result = image.copy()
+
+    image_threshold = ut.threshold(image)
+
+    # Generate different images for perspective warp 
+    height, width = image.shape[:2]
+    points = ut.trackbars_value()
+    image_warped = ut.perspective_warp(image_threshold, points, width, height)
+    image_points = ut.draw_points(image, points)
+
+    # Perform histogram analysis to find the lane curves
+    middle_point, image_histogram = ut.histogram_analysis(image_warped, display_chart=True, minimum_percentage=0.5, percentage_region_analyzed=25)
+    curve_average_point = ut.histogram_analysis(image_warped, minimum_percentage=0.9) # minimum percentage of 0.9 to find the most prominent lane line as the initial point
+    raw_curve = curve_average_point - middle_point
+
+    # Take average of the curve to smooth it out and eliminate noise
+    curve_list.append(raw_curve)
+    if len(curve_list) > average_curve_value:
+        curve_list.pop(0)
+    average_curve = int(np.mean(curve_list))
+
+    # Display the results based on the display mode
+    if display != 0:
+        image_inverse_warped = ut.perspective_warp(image_warped, points, width, height, inverse=True)
+        image_inverse_warped = cv2.cvtColor(image_inverse_warped, cv2.COLOR_GRAY2BGR)
+        image_inverse_warped[0:height // 3, 0:width] = 0, 0, 0
+        image_lane_color = np.zeros_like(image)
+        image_lane_color[:] = 0, 255, 0
+        image_lane_color = cv2.bitwise_and(image_inverse_warped, image_lane_color)
+        image_result = cv2.addWeighted(image_result, 1, image_lane_color, 1, 0)
+        middle_y = 450
+        cv2.putText(image_result, str(average_curve), (width // 2 - 80, 85), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
+        cv2.line(image_result, (width // 2, middle_y), (width // 2 + (average_curve * 3), middle_y), (255, 0, 255), 5)
+        cv2.line(image_result, ((width // 2 + (average_curve * 3)), middle_y - 25), (width // 2 + (average_curve * 3), middle_y + 25), (0, 255, 0), 5)
+        for x in range(-30, 30):
+            w = width // 20
+            cv2.line(image_result, (w * x + int(average_curve // 50), middle_y - 10),
+                     (w * x + int(average_curve // 50), middle_y + 10), (0, 0, 255), 2)
+        #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        #cv2.putText(imgResult, 'FPS ' + str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230, 50, 50), 3);
+    if display == 2:
+        image_stacked = ut.stack_images(0.7, ([image, image_points, image_warped],
+                                             [image_histogram, image_lane_color, image_result]))
+        cv2.imshow('ImageStack', image_stacked)
+    elif display == 1:
+        cv2.imshow('Resutlt', image_result)
+ 
+    #### NORMALIZATION
+    average_curve = average_curve/100
+    if average_curve>1: average_curve ==1
+    if average_curve<-1:average_curve == -1
+ 
+    return average_curve
+
+if __name__ == "__main__":
+    capture = cv2.VideoCapture("vid1.mp4")
+
+    initial_trackbar_values = [53, 204, 78, 131]  # Initial values for the trackbars
+    ut.initialize_trackbars(initial_trackbar_values)
+    frame_counter = 0
+
+    while True:
+        frame_counter += 1
+        if capture.get(cv2.CAP_PROP_FRAME_COUNT) == frame_counter:
+            capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            frame_counter = 0
+
+        success, image = capture.read()
+        image = cv2.resize(image, (480, 240))
+        get_lane_curve(image) 
+
+        cv2.waitKey(1)
---
+++ Line_Tracing/utilities.py
@@ -0,0 +1,171 @@
+import cv2
+import numpy as np
+
+def nothing(a):
+    pass
+
+def threshold(image):
+    """
+    Description: converts an image to HSV and applies a threshold to isolate white colors.
+    Parameters:
+    - image: Input image in BGR format.
+    """
+
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define thresholds for white color in HSV
+    lower_white = np.array([80, 0, 0])
+    upper_white = np.array([255, 160, 255])
+    # lower_white = np.array([92, 0, 16])
+    # upper_white = np.array([148, 255, 126])
+    mask_white = cv2.inRange(image_hsv, lower_white, upper_white)
+
+    return mask_white
+
+def perspective_warp(image, points, width, height, inverse=False):
+    """
+    Description: Applies a perspective warp to the image based on the given points.
+    Parameters:
+    - image: Input image in BGR format.
+    - points: List of four points defining the region to warp.
+    - width: Width of the output image.
+    - height: Height of the output image.
+    - inverse: If True, applies the inverse perspective warp.
+    """
+
+    source_points = np.float32(points)
+    destination_points = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+    if inverse:
+        matrix = cv2.getPerspectiveTransform(destination_points, source_points)
+    else:
+        matrix = cv2.getPerspectiveTransform(source_points, destination_points)
+    warped_image = cv2.warpPerspective(image, matrix, (width, height))
+
+    return warped_image
+
+def initialize_trackbars(trackbar_value, width_target=480, height_target=240):
+    """
+    Description: Initializes trackbars for HSV values.
+    Parameters:
+    - trackbar_value: Initial value for the trackbars.
+    - width_target: Target width for the perspective warp.
+    - height_target: Target height for the perspective warp.
+    """
+
+    cv2.namedWindow("Perspective Warp")
+    cv2.resizeWindow("Perspective Warp", width_target, height_target)
+    
+    cv2.createTrackbar("Width Top", "Perspective Warp", trackbar_value[0], width_target // 2, nothing)
+    cv2.createTrackbar("Height Top", "Perspective Warp", trackbar_value[1], height_target, nothing)
+    cv2.createTrackbar("Width Bottom", "Perspective Warp", trackbar_value[2], width_target // 2, nothing)
+    cv2.createTrackbar("Height Bottom", "Perspective Warp", trackbar_value[3], height_target, nothing)
+
+def trackbars_value(width_target=480, height_target=240):
+    """
+    Description: Retrieves the current values from the trackbars.
+    Parameters:
+    - width_target: Target width for the perspective warp.
+    - height_target: Target height for the perspective warp.
+    """
+
+    width_top = cv2.getTrackbarPos("Width Top", "Perspective Warp")
+    height_top = cv2.getTrackbarPos("Height Top", "Perspective Warp")
+    width_bottom = cv2.getTrackbarPos("Width Bottom", "Perspective Warp")
+    height_bottom = cv2.getTrackbarPos("Height Bottom", "Perspective Warp")
+
+    points = np.float32([
+        [width_top, height_top], 
+        [width_target - width_top, height_top], 
+        [width_bottom, height_bottom], 
+        [width_target - width_bottom, height_bottom]
+    ])
+
+    return points
+
+def draw_points(image, points):
+    """
+    Description: Draws the points on the image for visualization.
+    Parameters:
+    - image: Input image in BGR format.
+    - points: List of four points to draw.
+    """
+    image = image.copy()
+
+    for i in range(4):
+        cv2.circle(image, (int(points[i][0]), int(points[i][1])), 15, (0, 255, 0), cv2.FILLED)
+
+    return image
+
+def histogram_analysis(image, minimum_percentage=0.1, display_chart=False, percentage_region_analyzed=100):
+    """
+    Description: Analyzes the histogram of the image to find the lane lines.
+    Parameters:
+    - image: Input warped image
+    - minimum_percentage: Minimum percentage of pixels to consider a lane line.
+    - display_chart: Whether to display the histogram chart.
+    """
+
+    if percentage_region_analyzed == 1:
+        histogram_values = np.sum(image, axis=0)
+    else:
+        histogram_values = np.sum(image[int(image.shape[0] * percentage_region_analyzed / 100):, :], axis=0)
+
+    max_value = np.max(histogram_values)
+    minimum_value = max_value * minimum_percentage
+
+    line_index = np.where(histogram_values >= minimum_value)
+    base_point = int(np.average(line_index))
+    
+    if display_chart:
+        image_histogram = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+        for i, intensity in enumerate(histogram_values):
+            cv2.line(image_histogram, (i, image.shape[0]), (i, int(image.shape[0] - intensity // 255 * percentage_region_analyzed / 100)), (255, 0, 0), 1)
+            cv2.circle(image_histogram, (base_point, image.shape[0]), 20, (0, 255, 0), cv2.FILLED)
+
+        return base_point, image_histogram
+    
+    return base_point
+
+def stack_images(scale, image_array):
+    """
+    Description: Stacks images in a grid format.
+    Parameters:
+    - scale: Scale factor for resizing the images.
+    - image_array: 2D list of images to stack.
+    """
+
+    rows = len(image_array)
+    cols = len(image_array[0])
+    rows_available = isinstance(image_array[0], list)
+    
+    width = image_array[0][0].shape[1]
+    height = image_array[0][0].shape[0]
+
+    if rows_available:
+        for i in range(rows):
+            for j in range(cols):
+                if image_array[i][j].shape[:2] == image_array[0][0].shape[:2] :
+                    image_array[i][j] = cv2.resize(image_array[i][j], (0,0), None, scale, scale)
+                else:
+                    image_array[i][j] = cv2.resize(image_array[i][j], (image_array[0][0].shape[1], image_array[0][0].shape[0]), None, scale, scale)
+            
+                if len(image_array[i][j].shape) == 2: image_array[i][j] = cv2.cvtColor(image_array[i][j], cv2.COLOR_GRAY2BGR)                    
+        blank_image = np.zeros((height, width, 3), np.uint8)
+        horizontal = [blank_image] * rows
+        for x in range(rows):
+            horizontal[x] = np.hstack(image_array[x])
+        vertical = np.vstack(horizontal)
+    else:
+        for x in range(rows):
+            if image_array[x].shape[:2] == image_array[0].shape[:2]:
+                image_array[x] = cv2.resize(image_array[x], (0,0), None, scale, scale)
+            else:
+                image_array[x] = cv2.resize(image_array[x], (image_array[0].shape[1], image_array[0].shape[0]), None, scale, scale)
+            
+            if len(image_array[x].shape) == 2: image_array[x] = cv2.cvtColor(image_array[x], cv2.COLOR_GRAY2BGR)
+                
+        horizontal = np.hstack(image_array)
+        vertical = horizontal
+
+    return vertical
