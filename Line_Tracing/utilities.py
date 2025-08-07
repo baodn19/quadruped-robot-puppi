1ffa047acd26e05d4a0e771f51c02c6317c3025f
baodn19 <baodn19@gmail.com>
2025-08-07 21:19:55 +0700

    use value for robot

--- Line_Tracing/utilities.py
+++ Line_Tracing/utilities.py
@@ -14,10 +14,10 @@ def threshold(image):
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Define thresholds for white color in HSV
-    lower_white = np.array([80, 0, 0])
-    upper_white = np.array([255, 160, 255])
-    # lower_white = np.array([92, 0, 16])
-    # upper_white = np.array([148, 255, 126])
+    # lower_white = np.array([80, 0, 0])
+    # upper_white = np.array([255, 160, 255])
+    lower_white = np.array([92, 0, 16])
+    upper_white = np.array([148, 255, 126])
     mask_white = cv2.inRange(image_hsv, lower_white, upper_white)
 
     return mask_white
